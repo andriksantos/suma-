83f15de7373ac283d533d738cc7a72fa0b70609c
Andrick Santos <andricksantos1@gmail.com>
2018-04-30 10:47:22 -0600

    first commit

---
+++ suma sencilla.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+//primer parcial
+using namespace std;
+
+int main(){
+	int n1,n2,suma=0;
+
+	cout<<"Ingrese el primer numero...:";
+	cin>>n1;
+	cout<<"Ingrese el segundo numero..:";
+	cin>>n2;
+
+	suma = n1 + n2;
+
+	cout<<"El resultado de la suma es...:" <<suma<<endl;
+
+	return 0;
+	system("PAUSE");
+}
